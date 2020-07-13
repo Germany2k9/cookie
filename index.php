9bb22c472b0bde4fcae9fb11926aeb6afafab764
geraliaga@aulanz.net <germany@MacBook-Pro.local>
2020-07-14 00:54:17 +0200

    set cookie

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php 
+    //setcookie sirve para crear la cookie 
+    //son ficheros que se guardan que tienen una caducidad
+    //programas la caducidad de las cookies.
+    //lo que pasa las cookies los ultimos anio es que guarda demasiada informacion..
+    // se establece una sesion y necesita de una cookie para poder funcionar 
+    //hasta que punto puede guardar informacion.
+    //siempre poner un valor ( time() ) no si no muere la cookie.
+    // "/" definimos para todo el sitio o podes pasar un path /carpeta/index.php
+    //https si va por un protocolo seguro. true - false 
+    //https si va por un protocolo normal. true - false 
+
     setcookie("contador", 0, time()+ 60*60*24*365, "/" ,false , true ); "i" ; 
+    //leer el valor de una cookie
+
+    //$cookie=$_COOKIE['contador'];   
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
